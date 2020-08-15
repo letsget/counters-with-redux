c5fee9bc4cb694f24bec24aeb7c0ea417ab821e0
letsget <andrewbond25@gmail.com>
2020-08-15 21:06:57 +0200

    Updates

---
+++ precache-manifest.542e743a90078dc37e56eb4ae3bd39cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e62f4f6589b749962c4ab98d75b4c9b",
+    "url": "/counters-with-redux/index.html"
+  },
+  {
+    "revision": "7551fb304b1211dcffe5",
+    "url": "/counters-with-redux/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "a1c863f6f2085ffae258",
+    "url": "/counters-with-redux/static/css/main.345f2380.chunk.css"
+  },
+  {
+    "revision": "7551fb304b1211dcffe5",
+    "url": "/counters-with-redux/static/js/2.94618017.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/counters-with-redux/static/js/2.94618017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1c863f6f2085ffae258",
+    "url": "/counters-with-redux/static/js/main.58e18bd6.chunk.js"
+  },
+  {
+    "revision": "38bb7520226817079a2a",
+    "url": "/counters-with-redux/static/js/runtime-main.fd8f6688.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.46b25877d65a3be5b78870885eb70f76.js"
+  "/counters-with-redux/precache-manifest.542e743a90078dc37e56eb4ae3bd39cb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/counters-with-redux/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
